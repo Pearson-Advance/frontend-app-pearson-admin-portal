8703e22994598d27efc24882605adbcda569ec6a
Elvin Quero <elvin.quero@edunext.co>
2021-12-06 07:11:35 -0400

    feat: add modal to add institutions.
    feat: comments were addressed.

---
+++ src/features/institutions/components/institutionForm/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Form } from '@edx/paragon';
+import PropTypes from 'prop-types';
+
+export const InstitutionForm = ({ fields, setFields, errors }) => {
+  const handleInputChange = (e) => {
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <Form.Group isInvalid={Object.prototype.hasOwnProperty.call(errors, 'name')}>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          name="name"
+          maxLength="64"
+          value={fields.name}
+          onChange={handleInputChange}
+        />
+        {errors.name && <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>}
+      </Form.Group>
+      <Form.Group isInvalid={Object.prototype.hasOwnProperty.call(errors, 'shortName')}>
+        <Form.Label>Short name</Form.Label>
+        <Form.Control
+          name="shortName"
+          maxLength="20"
+          value={fields.shortName}
+          onChange={handleInputChange}
+        />
+        {errors.shortName && <Form.Control.Feedback type="invalid">{errors.shortName}</Form.Control.Feedback>}
+      </Form.Group>
+      <Form.Group>
+        <Form.Checkbox
+          name="active"
+          checked={fields.active}
+          onChange={() => setFields({ ...fields, active: !fields.active })}
+        >
+          Active
+        </Form.Checkbox>
+      </Form.Group>
+    </>
+  );
+};
+
+InstitutionForm.propTypes = {
+  fields: PropTypes.shape({
+    name: PropTypes.string,
+    shortName: PropTypes.string,
+    active: PropTypes.bool,
+  }).isRequired,
+  setFields: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    name: PropTypes.string,
+    shortName: PropTypes.string,
+  }).isRequired,
+};
