fa22edc384755a7e4c810d050f75fb66ef4cb2b6
Ivan Gonzales <ivanv.gonzales@gmail.com>
2021-12-27 12:11:10 -0500

    feat: license filters

--- src/features/licenses/components/LicenseTable/index.jsx
+++ src/features/licenses/components/LicenseTable/index.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useMemo } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { PersistController } from 'features/shared/components/PersistController';
 import { fetchLicenses } from 'features/licenses/data';
-import { DataTable } from '@edx/paragon';
+import { DataTable, TextFilter } from '@edx/paragon';
 import { getColumns } from './columns';
 
 const LicenseTable = () => {
   const dispatch = useDispatch();
-
+  const {
+    pageSize, pageIndex, filters, sortBy,
+  } = useSelector(state => state.page.dataTable);
   const columns = useMemo(() => getColumns(), []);
   const { data } = useSelector(state => state.licenses);
 
@@ -17,23 +20,24 @@ const LicenseTable = () => {
   }, [dispatch]);
 
   return (
-    <div className="pgn__data-table-wrapper mb-4">
-      <DataTable
-        isSortable
-        isPaginated
-        initialState={{
-          pageSize: 10,
-          pageIndex: 0,
-        }}
-        itemCount={data.length}
-        data={data}
-        columns={columns}
-      >
-        <DataTable.Table />
-        <DataTable.EmptyTable content="No results found." />
-        <DataTable.TableFooter />
-      </DataTable>
-    </div>
+    <DataTable
+      isSortable
+      isPaginated
+      isFilterable
+      showFiltersInSidebar
+      defaultColumnValues={{ Filter: TextFilter }}
+      initialState={{
+        pageSize, pageIndex, filters: JSON.parse(filters), sortBy,
+      }}
+      itemCount={data.length}
+      data={data}
+      columns={columns}
+    >
+      <DataTable.Table />
+      <DataTable.EmptyTable content="No results found." />
+      <DataTable.TableFooter />
+      <PersistController />
+    </DataTable>
   );
 };
 
