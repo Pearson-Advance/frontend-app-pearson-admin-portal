e3411bc8475124ec81c22a2a3f2f6f240b566d95
Ivan Gonzales <ivanv.gonzales@gmail.com>
2022-01-04 10:57:52 -0500

    feat: add License.

---
+++ src/features/licenses/components/LicenseForm/index.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Icon, PageBanner } from '@edx/paragon';
+import PropTypes from 'prop-types';
+import { activeInstitutions } from 'features/institutions/data/selector';
+import { has } from 'lodash';
+import Select from 'react-select';
+import { WarningFilled } from '@edx/paragon/icons';
+
+export const LicenseForm = ({ fields, setFields, errors }) => {
+  const data = useSelector(activeInstitutions);
+  const { form } = useSelector(state => state.licenses);
+
+  const handleInputChange = (e) => {
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSelectCourseChange = (selected) => {
+    setFields({
+      ...fields,
+      course: selected ? selected.value : '',
+    });
+  };
+
+  return (
+    <>
+      {has(errors, 'nonFieldErrors')
+        && (
+        <Form.Group>
+          <PageBanner
+            variant="warning"
+          >
+            <Icon src={WarningFilled} className="mr-2" /> {errors.nonFieldErrors}
+          </PageBanner>
+        </Form.Group>
+        )}
+
+      <Form.Group>
+        <Form.Control
+          name="institution"
+          floatingLabel="Institution"
+          as="select"
+          required
+          isInvalid={has(errors, 'institution') && has(errors.institution, 'id')}
+          onChange={handleInputChange}
+        >
+          <option value="">Choose...</option>
+          {data.map(institution => <option key={`institution-${institution.id}`} value={institution.id}> {institution.name} </option>)}
+        </Form.Control>
+        {errors.institution && errors.institution.id && <Form.Control.Feedback type="invalid">{errors.institution.id}</Form.Control.Feedback>}
+      </Form.Group>
+      <Form.Group isInvalid={has(errors, 'course') && has(errors.course, 'id')} className="mb-3">
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          placeholder="Choose a master course..."
+          isDisabled={false}
+          isLoading={false}
+          isClearable
+          isRtl={false}
+          isSearchable
+          options={form.managedCourses}
+          maxMenuHeight={150}
+          name="course"
+          onChange={handleSelectCourseChange}
+        />
+        {errors.course && errors.course.id && <Form.Control.Feedback type="invalid">{errors.course.id}</Form.Control.Feedback>}
+      </Form.Group>
+      <Form.Group>
+        <Form.Control
+          name="courseAccessDuration"
+          floatingLabel="Course access duration"
+          value={fields.courseAccessDuration}
+          onChange={handleInputChange}
+        />
+        {errors.courseAccessDuration && <Form.Control.Feedback type="invalid">{errors.courseAccessDuration}</Form.Control.Feedback>}
+      </Form.Group>
+      <Form.Group>
+        <Form.Control
+          as="select"
+          name="status"
+          floatingLabel="Status"
+          defaultValue="active"
+          isInvalid={Object.prototype.hasOwnProperty.call(errors, 'status')}
+          onChange={handleInputChange}
+        >
+          <option value="active">active</option>
+          <option value="disabled">disabled</option>
+        </Form.Control>
+        {errors.status && <Form.Control.Feedback type="invalid">{errors.status}</Form.Control.Feedback>}
+      </Form.Group>
+    </>
+  );
+};
+
+LicenseForm.propTypes = {
+  fields: PropTypes.shape({
+    institution: PropTypes.string,
+    course: PropTypes.string,
+    courseAccessDuration: PropTypes.number,
+    status: PropTypes.string,
+  }).isRequired,
+  setFields: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    institution: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+    course: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+    courseAccessDuration: PropTypes.number,
+    status: PropTypes.string,
+    nonFieldErrors: PropTypes.string,
+  }).isRequired,
+};
--- src/features/licenses/data/thunks.js
+++ src/features/licenses/data/thunks.js
@@ -1,6 +1,9 @@
 import { logError } from '@edx/frontend-platform/logging';
 import { camelCaseObject } from '@edx/frontend-platform';
-import { getLicenses, getLicenseById } from './api';
+import { startsWith } from 'lodash';
+import {
+  getLicenses, getLicenseById, postLicense, getLicenseManageCourses,
+} from './api';
 import {
   fetchLicensesRequest,
   fetchLicensesSuccess,
@@ -8,6 +11,11 @@ import {
   fetchLicenseRequest,
   fetchLicenseSuccess,
   fetchLicenseFailed,
+  postLicenseSuccess,
+  postLicenseFailed,
+  fetchLicenseManageCoursesRequest,
+  fetchLicenseManageCoursesSuccess,
+  fetchLicenseManageCoursesFailed,
 } from './slices';
 
 /**
@@ -37,6 +45,52 @@ export function fetchLicensebyId(id) {
       dispatch(fetchLicenseSuccess(camelCaseObject((await getLicenseById(id)).data)));
     } catch (error) {
       dispatch(fetchLicenseFailed());
+    }
+  };
+}
+
+/** Post License creation.
+ * @returns {(function(*): Promise<void>)|*}
+ */
+export function createLicense(institution, course, courseAccessDuration, status) {
+  return async (dispatch) => {
+    try {
+      dispatch(
+        postLicenseSuccess(
+          camelCaseObject(
+            (await postLicense(
+              institution,
+              course,
+              courseAccessDuration,
+              status,
+            )).data,
+          ),
+        ),
+      );
+    } catch (error) {
+      dispatch(postLicenseFailed(camelCaseObject(error.response.data)));
+      logError(error);
+    }
+  };
+}
+
+/**
+ * Fetches all license managed courses.
+ * @returns {(function(*): Promise<void>)|*}
+ */
+export function fetchLicenseManageCourses(url) {
+  return async (dispatch) => {
+    try {
+      const response = (await getLicenseManageCourses(url)).data;
+
+      dispatch(fetchLicenseManageCoursesRequest());
+      dispatch(
+        fetchLicenseManageCoursesSuccess(
+          camelCaseObject(response.results.filter(course => startsWith(course.id, 'course-v1'))),
+        ),
+      );
+    } catch (error) {
+      dispatch(fetchLicenseManageCoursesFailed());
       logError(error);
     }
   };
