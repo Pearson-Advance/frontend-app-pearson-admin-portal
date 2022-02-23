717093aed4c1cc03fb0ed1a40acce2e1155f6f27
Javier <javier.torres@edunext.co>
2022-02-23 09:38:38 -0500

    feat: add Master Course level report in data report tab.

---
+++ src/features/DataReport/components/LicenseUsageMCLevel/Table.jsx
@@ -0,0 +1,44 @@
+import { DataTable } from '@edx/paragon';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export const Table = ({ data, count }) => {
+  const columns = [
+    {
+      Header: 'Institution',
+      accessor: 'institution',
+    },
+    {
+      Header: 'Master Course',
+      accessor: 'masterCourse',
+    },
+    {
+      Header: 'Purchased seats',
+      accessor: 'purchasedSeats',
+    },
+    {
+      Header: 'Total Enrolled',
+      accessor: 'totalEnrolled',
+    },
+  ];
+  return (
+    <DataTable
+      itemCount={count}
+      data={data}
+      columns={columns}
+    >
+      <DataTable.Table />
+      <DataTable.EmptyTable content="No data found." />
+      <DataTable.TableFooter />
+    </DataTable>
+  );
+};
+
+Table.propTypes = {
+  count: PropTypes.number.isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape([])),
+};
+
+Table.defaultProps = {
+  data: [],
+};
--- src/features/DataReport/data/api.js
+++ src/features/DataReport/data/api.js
@@ -9,6 +9,13 @@ function getLicenseUsageCCXLevel(filters) {
   );
 }
 
+function getLicenseUsageMCLevel(filters) {
+  return getAuthenticatedHttpClient().get(
+    `${process.env.COURSE_OPERATIONS_API_BASE_URL}/license-usage/`,
+    { params: snakeCaseObject(removeNullOrEmptyObjectAttributes(filters)) },
+  );
+}
+
 function getExportLicenseUsageCCXLevel(filters) {
   let params = {};
 
@@ -21,7 +28,21 @@ function getExportLicenseUsageCCXLevel(filters) {
   );
 }
 
+function getExportLicenseUsageMCLevel(filters) {
+  let params = {};
+
+  if (filters) {
+    params = snakeCaseObject(removeNullOrEmptyObjectAttributes(filters));
+  }
+
+  return getAuthenticatedHttpClient().get(
+    `${process.env.COURSE_OPERATIONS_API_BASE_URL}/license-usage-export/`, { params }, { responseType: 'blob' },
+  );
+}
+
 export {
   getLicenseUsageCCXLevel,
   getExportLicenseUsageCCXLevel,
+  getLicenseUsageMCLevel,
+  getExportLicenseUsageMCLevel,
 };
